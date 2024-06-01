30bdde50140d2d87aeb13b3974a3e1ea848000fb
JwanCortez93 <jwancortez@gmail.com>
2024-06-01 15:08:10 -0300

    Category scroll completed

--- src/components/CategoryItems.tsx
+++ src/components/CategoryItems.tsx
@@ -1,6 +1,6 @@
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import { Button } from "./Button";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 type CategoryItemsProps = {
   categories: string[];
@@ -8,16 +8,42 @@ type CategoryItemsProps = {
   onSelect: (category: string) => void;
 };
 
+const TRANSLATE_PIXELS = 200;
+
 export function CategoryItems({
   categories,
   selectedCategory,
   onSelect,
 }: CategoryItemsProps) {
+  const [translate, setTranslate] = useState(0);
   const [isLeftVisible, setIsLeftVisible] = useState(false);
-  const [isRightVisible, setIsRightVisible] = useState(false);
+  const [isRightVisible, setIsRightVisible] = useState(true);
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (containerRef.current == null) return;
+    const observer = new ResizeObserver((entries) => {
+      const container = entries[0].target;
+      if (container == null) return;
+
+      setIsLeftVisible(translate > 0);
+      setIsRightVisible(
+        translate + container.clientWidth < container.scrollWidth
+      );
+    });
+
+    observer.observe(containerRef.current);
+    return () => {
+      observer.disconnect();
+    };
+  }, [categories, translate]);
+
   return (
-    <div className="overflow-x-hidden relative">
-      <div className="flex whitespace-nowrap gap-3 transition-transform w-[max-content]">
+    <div ref={containerRef} className="overflow-x-hidden relative">
+      <div
+        className="flex whitespace-nowrap gap-3 transition-transform w-[max-content]"
+        style={{ transform: `translateX(-${translate}px)` }}
+      >
         {categories.map((category) => (
           <Button
             onClick={() => onSelect(category)}
@@ -32,6 +58,15 @@ export function CategoryItems({
       {isLeftVisible && (
         <div className="absolute left-0 top-1/2 -translate-y-1/2 bg-gradient-to-r from-white from-50% to-transparent w-24 h-full">
           <Button
+            onClick={() => {
+              setTranslate((translate) => {
+                const newTranslate = translate - TRANSLATE_PIXELS;
+                if (newTranslate <= 0) {
+                  return 0;
+                }
+                return newTranslate;
+              });
+            }}
             variant={"ghost"}
             size={"icon"}
             className="h-full aspect-square w-auto p-1.5"
@@ -46,6 +81,18 @@ export function CategoryItems({
             variant={"ghost"}
             size={"icon"}
             className="h-full aspect-square w-auto p-1.5"
+            onClick={() => {
+              setTranslate((translate) => {
+                if (containerRef.current == null) return translate;
+                const newTranslate = translate + TRANSLATE_PIXELS;
+                const edge = containerRef.current.scrollWidth;
+                const width = containerRef.current.clientWidth;
+                if (newTranslate + width >= edge) {
+                  return edge - width;
+                }
+                return newTranslate;
+              });
+            }}
           >
             <ChevronRight />
           </Button>
