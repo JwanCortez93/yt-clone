663176fb843b5be996d8eab534978312d24f3c27
JwanCortez93 <jwancortez@gmail.com>
2024-06-01 14:38:13 -0300

    added mock data to work with

---
+++ src/data/home.ts
@@ -0,0 +1,148 @@
+export const categories = [
+  "All",
+  "Travel",
+  "Cooking",
+  "Gaming",
+  "Fitness",
+  "Technology",
+  "Education",
+  "Music",
+  "Comedy",
+  "Lifestyle",
+  "DIY",
+  "Health",
+  "Science",
+  "History",
+  "Art",
+];
+
+export const videos = [
+  {
+    id: "1",
+    title: "Exploring the Hidden Gems of Japan",
+    channel: {
+      name: "Travel Guru",
+      id: "TravelGuru",
+      profileUrl:
+        "https://yt3.ggpht.com/ytc/APkrFKZWeMCsx4Q9e_Hm6nhOOUQ3fv96QGUXiMr1-pPP=s48-c-k-c0x00ffffff-no-rj",
+    },
+    views: 1203456,
+    postedAt: new Date("2023-05-14"),
+    duration: 1234,
+    thumbnailUrl: "https://i.ytimg.com/vi/x7BCUrrggFI/maxresdefault.jpg",
+    videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+  },
+  {
+    id: "2",
+    title: "The Ultimate Guide to Making Pasta from Scratch",
+    channel: {
+      name: "Chef's Table",
+      id: "ChefsTable",
+      profileUrl:
+        "https://yt3.ggpht.com/ytc/APkrFKb--NH6RwAGHYsD3KfxX-SAgWgIHrjR5E4Jb5SDSQ=s48-c-k-c0x00ffffff-no-rj",
+    },
+    views: 985412,
+    postedAt: new Date("2023-06-23"),
+    duration: 1567,
+    thumbnailUrl: "https://i.ytimg.com/vi/3pBAYEMI7B0/maxresdefault.jpg",
+    videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
+  },
+  {
+    id: "3",
+    title: "Top 10 Tips for Mastering Fortnite",
+    channel: {
+      name: "Gamer's Paradise",
+      id: "GamersParadise",
+      profileUrl:
+        "https://yt3.ggpht.com/ytc/APkrFKa6XiLa13mMVPzkmmTBcgNPjjqCGPrY86KfJFmf5w=s48-c-k-c0x00ffffff-no-rj",
+    },
+    views: 5401234,
+    postedAt: new Date("2023-04-15"),
+    duration: 987,
+    thumbnailUrl: "https://i.ytimg.com/vi/NH5B0q0W5_U/maxresdefault.jpg",
+    videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/Sintel.mp4",
+  },
+  {
+    id: "4",
+    title: "Full Body Workout Routine for Beginners",
+    channel: {
+      name: "Fit Life",
+      id: "FitLife",
+      profileUrl:
+        "https://yt3.ggpht.com/ytc/APkrFKa6XiLa13mMVPzkmmTBcgNPjjqCGPrY86KfJFmf5w=s48-c-k-c0x00ffffff-no-rj",
+    },
+    views: 873456,
+    postedAt: new Date("2023-03-29"),
+    duration: 1432,
+    thumbnailUrl: "https://i.ytimg.com/vi/Y5kWfypgoE4/maxresdefault.jpg",
+    videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4",
+  },
+  {
+    id: "5",
+    title: "Latest Tech Gadgets in 2023",
+    channel: {
+      name: "Tech Savvy",
+      id: "TechSavvy",
+      profileUrl:
+        "https://yt3.ggpht.com/ytc/APkrFKZWeMCsx4Q9e_Hm6nhOOUQ3fv96QGUXiMr1-pPP=s48-c-k-c0x00ffffff-no-rj",
+    },
+    views: 234789,
+    postedAt: new Date("2023-07-08"),
+    duration: 1298,
+    thumbnailUrl: "https://i.ytimg.com/vi/0v2aq0AIM2U/maxresdefault.jpg",
+    videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+  },
+  {
+    id: "6",
+    title: "How to Ace Your Next Interview",
+    channel: {
+      name: "Career Coach",
+      id: "CareerCoach",
+      profileUrl:
+        "https://yt3.ggpht.com/ytc/APkrFKZWeMCsx4Q9e_Hm6nhOOUQ3fv96QGUXiMr1-pPP=s48-c-k-c0x00ffffff-no-rj",
+    },
+    views: 567123,
+    postedAt: new Date("2023-02-20"),
+    duration: 786,
+    thumbnailUrl: "https://i.ytimg.com/vi/Ke90Tje7VS0/maxresdefault.jpg",
+    videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
+  },
+  {
+    id: "7",
+    title: "10 Minute Yoga Routine for Beginners",
+    channel: {
+      name: "Healthy Mind",
+      id: "HealthyMind",
+      profileUrl:
+        "https://yt3.ggpht.com/ytc/APkrFKa6XiLa13mMVPzkmmTBcgNPjjqCGPrY86KfJFmf5w=s48-c-k-c0x00ffffff-no-rj",
+    },
+    views: 345678,
+    postedAt: new Date("2023-01-12"),
+    duration: 600,
+    thumbnailUrl: "https://i.ytimg.com/vi/9p8VfZ4fF4w/maxresdefault.jpg",
+    videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/Sintel.mp4",
+  },
+  {
+    id: "8",
+    title: "The History of Ancient Rome",
+    channel: {
+      name: "History Buff",
+      id: "HistoryBuff",
+      profileUrl:
+        "https://yt3.ggpht.com/ytc/APkrFKZWeMCsx4Q9e_Hm6nhOOUQ3fv96QGUXiMr1-pPP=s48-c-k-c0x00ffffff-no-rj",
+    },
+    views: 789012,
+    postedAt: new Date("2023-08-05"),
+    duration: 2100,
+    thumbnailUrl: "https://i.ytimg.com/vi/aBvLXtY3Xzk/maxresdefault.jpg",
+    videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4",
+  },
+];
